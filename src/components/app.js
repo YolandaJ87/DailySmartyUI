1a6a32e2bf6bb015b9a2e67aa8a3864080fc70d9
YolandaJ87 <yolanda_j87@hotmail.com>
2020-11-22 11:55:03 +0100

    logo pagina principal

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>logo</div>
+        <div className="logo">
+          <div>
+            <Logo />
+          </div>
+        </div>
+
+        <div className="search">
+          <input placeholder="daily smarty"/>
+        </div>
+
+        <div className="recent">
+          <ul>
+            <li>post</li>
+            <li>post</li>
+            <li>post</li>
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class logo extends Component{
+        render (){
+            return (
+                <div className="logo-main"> 
+                    <img alt="logo daily smarty (grande)" src="/assets/ds_circle_logo.png"/>
+                </div>
+            );
+        };
+    }
