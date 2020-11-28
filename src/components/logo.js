fbb42445c87b7ba247f8369a4ba7869e430b7288
YolandaJ87 <yolanda_j87@hotmail.com>
2020-11-28 10:20:09 +0100

    logo.js estilo automatico dependiendo de donde se le llame

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 export default class logo extends Component{
-        render (){
-            return (
-                <div className="logo-main"> 
-                    <img alt="logo daily smarty (grande)" src="/assets/ds_circle_logo.png"/>
-                </div>
-            );
-        };
-    }
+    render (){
+        const size = {
+            height: this.props.size ? this.props.size : 105,
+            width: this.props.size ? this.props.size : 105
+        }
+        return (
+            <div className="logo-main"> 
+                <img style={size} alt="logo daily smarty (grande)" src="/assets/ds_circle_logo.png"/>
+            </div>
+        );
+    };
+}
